794907cdccdfc0b7493c2f64cd980705adeab0c0
Jan Greger Hemb <jan.greger@gmail.com>
2019-05-14 14:11:22 +0200

    Changed to new project (backend)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyDrmdyp4tQdqXZ07bxLfavkBppviHGADdE",
-    authDomain: "digital-escape-room.firebaseapp.com",
-    databaseURL: "https://digital-escape-room.firebaseio.com",
-    projectId: "digital-escape-room",
-    storageBucket: "digital-escape-room.appspot.com",
-    messagingSenderId: "678141850024"
+    apiKey: 'AIzaSyDq7p69Lk-xHXH02l4_T28swqI76OQfMPY',
+    authDomain: 'digitalescaperoom2019.firebaseapp.com',
+    databaseURL: 'https://digitalescaperoom2019.firebaseio.com',
+    projectId: 'digitalescaperoom2019',
+    storageBucket: 'digitalescaperoom2019.appspot.com',
+    messagingSenderId: '216000037056',
+    appId: '1:216000037056:web:8728b0dacc92e9ed'
   }
 };
